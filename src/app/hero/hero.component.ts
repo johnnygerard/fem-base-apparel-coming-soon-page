fdfcca0836a6a02ab8acae53d362b225c053430c
Johnny Gérard <mail@jgerard.dev>
2023-04-21 21:45:14 +0200

    feat: hero image

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./hero.component.scss']
 })
 export class HeroComponent {
+  private readonly ikBaseURL = 'https://ik.imagekit.io/jgerard/fem-base-apparel-coming-soon-page';
+  private readonly breakpoints = [16, 32, 48, 64, 96, 128, 256, 384, 640, 750, 828, 1080, 1200, 1920, 2048, 3840];
 
+  getSrcSet(src: string): string {
+    return this.breakpoints
+      .map(breakpoint => `${this.ikBaseURL}/tr:w-${breakpoint}/${src} ${breakpoint}w`)
+      .join(', ');
+  }
 }

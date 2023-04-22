b029c7da7d0804655bf117a518e7f6763d0897a7
Johnny Gérard <mail@jgerard.dev>
2023-04-22 09:37:23 +0200

    feat: generate svg error component

---
+++ src/app/svg-error/svg-error.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'svg-error',
+  templateUrl: './svg-error.component.html',
+  styleUrls: ['./svg-error.component.scss']
+})
+export class SvgErrorComponent {
+
+}
